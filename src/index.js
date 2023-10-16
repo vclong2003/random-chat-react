716e41962caeb5337f68c45ef91a899c26bf38ce
vclong2003 <vclong2003@gmail.com>
2023-10-16 11:57:29 +0700

    backUp

--- src/index.js
+++ src/index.js
@@ -1,42 +1,94 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import { Container, Form } from "react-bootstrap";
+import { io } from "socket.io-client";
 import styles from "./styles.module.css";
+
+const socket = io("http://localhost:3001");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+socket.on("connect", () => {
+  console.log("Connected");
+});
+
+function App() {
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState("");
+
+  const msgEnd = useRef(null);
+
+  const handleSendMessage = (evt) => {
+    evt.preventDefault();
+
+    if (message === "") return;
+
+    socket.emit("send-message", { msg: message });
+    setMessage("");
+  };
+
+  useEffect(() => {
+    socket.on("receive-message", (msg) => {
+      setMessages(msg);
+    });
+
+    return () => {
+      socket.off("receive-message");
+    };
+  });
+
+  useEffect(() => {
+    msgEnd.current.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
+
+  return (
     <Container fluid className={styles.container}>
       <Container fluid className={styles.messageContainer}>
         {/* Message */}
-        <MessageItem />
-        <MessageItem />
+        {messages.map((msg, index) => {
+          return (
+            <MessageItem
+              key={index}
+              sender={msg.sender}
+              time={msg.time}
+              content={msg.content}
+            />
+          );
+        })}
+        <div ref={msgEnd} />
       </Container>
-      <Form className={styles.textInputContainer}>
-        <input type="text" className={styles.msgInput} />
+      <Form className={styles.textInputContainer} onSubmit={handleSendMessage}>
+        <input
+          type="text"
+          className={styles.msgInput}
+          value={message}
+          onChange={(evt) => {
+            setMessage(evt.target.value);
+          }}
+        />
         <button type="submit" className={styles.sendBtn}>
           Send
         </button>
       </Form>
     </Container>
-  </React.StrictMode>
-);
+  );
+}
 
-function MessageItem() {
+function MessageItem({ sender, time, content }) {
   return (
     <Container fluid className={styles.message}>
       <div className={styles.msgInfo}>
-        <div>VCL</div>
-        <div>2023-10-09, 1605</div>
+        <div>{sender}</div>
+        <div>{time}</div>
       </div>
       <div style={{ width: "100%", height: "20px" }} />
-      <div className={styles.msgContent}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-        voluptates, nesciunt, quibusdam, voluptatum eaque quod consequatur
-        voluptate quia quae asperiores quos. Quisquam voluptates, nesciunt
-        voluptatum eaque quod consequatur voluptate quia quae asperiores quos.
-      </div>
+      <div className={styles.msgContent}>{content}</div>
     </Container>
   );
 }
